edd66e599e65e950fdaa3d869c76a8d04cc84d83
Teddy <117791879+TeddySyson@users.noreply.github.com>
2022-11-09 10:42:47 +0800

    Update streamlit_app.py
    added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Banana Peanut Butter Smoothie')
+streamlit.text('Soft-boiled egg')
