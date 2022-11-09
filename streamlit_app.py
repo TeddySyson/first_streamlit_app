c11ae8b7b58afcee973ddc61a0d0d218cfbdcdbc
Teddy <117791879+TeddySyson@users.noreply.github.com>
2022-11-09 10:51:32 +0800

    Update streamlit_app.py
    added emojis and fruit smoothie header

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Banana Peanut Butter Smoothie')
-streamlit.text('Soft-boiled egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Banana Peanut Butter Smoothie')
+streamlit.text('🐔 Soft-boiled egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
